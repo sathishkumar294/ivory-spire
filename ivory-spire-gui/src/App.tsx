f4860676aa9f8999912d243bbaeffa155a132dbf
Sathish Kumar <sathishkumar.aero51@gmail.com>
2025-03-29 01:06:27 +0100

    move to react app

--- ivory-spire-gui/src/App.tsx
+++ ivory-spire-gui/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+type Shortcut = {
+  title: string;
+  url: string;
+  icon: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [shortcuts, setShortcuts] = useState<Shortcut[]>([]);
+
+  useEffect(() => {
+    fetch("/shortcuts.json")
+      .then((response) => response.json())
+      .then((data) => setShortcuts(data.shortcuts));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="shortcuts-container">
+      <div className="name"> MAGILAN </div>{" "}
+      {shortcuts.map((shortcut) => (
+        <a href={shortcut.url} className="shortcut-card">
+          <img
+            src={shortcut.icon}
+            alt={shortcut.title}
+            className="shortcut-image"
+          />
+          <span className="shortcut-title"> {shortcut.title} </span>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
